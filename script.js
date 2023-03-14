6c1906fcc63453a4ad5f0b33b25719e24395bdc3
Ameer_105 <ameerdamilola@gmail.com>
2023-03-14 04:21:24 +0100

    added responsiveness to header

--- script.js
+++ script.js
@@ -956,3 +956,15 @@ tailwind.config = {
       }
     }
   }
+
+  const burger = document.querySelector('#burger');
+  const menu = document.querySelector('#menu');
+
+  burger.addEventListener('click', () => {
+    if (menu.classList.contains('hidden')) {
+      menu.classList.remove('hidden');
+    } else {
+      menu.classList.add('hidden');
+    
+    }
+  });
